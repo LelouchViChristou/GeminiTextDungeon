cf2c6a32facdebd38de5114af7e9839d0e484c82
Christou Lenos <christoulenos123@gmail.com>
2024-10-30 16:52:45 +0200

    Update api_setup.py

--- api_setup.py
+++ api_setup.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 
 # API setup
-API_KEY = "AIzaSyCPDvWZt7wZHMLrXtwbrcRl_AY_-WRnlNw"
+
 genai.configure(api_key=API_KEY)
 
 modelRoomGen = genai.GenerativeModel('gemini-1.5-flash', generation_config={"response_mime_type": "application/json"})
